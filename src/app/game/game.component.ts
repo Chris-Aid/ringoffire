dd1b8b7e406e60d25083849f5ceb5373421d2301
Christian Aidelsburger <c.aidelsburger@gmx.de>
2022-06-26 19:01:33 +0200

    added button to select a background image

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -57,14 +57,15 @@ export class GameComponent implements OnInit {
 
   startDealing() {
     // setTimeout(() => {
-    setInterval(() => {
+    let dealCards = setInterval(() => {
       if (this.game.dealingCards.length > 0) {
         let poppedCard = this.game.dealingCards.pop();
         this.game.dealedCards.push(poppedCard);
         console.log(poppedCard)
       } else {
         document.getElementById('topCard').style.display = "none";
-        // this.designCardsAfterDealing();
+        this.designCardsAfterDealing();
+        clearInterval(dealCards);
       }
     }, 200);
     // }, 500);
@@ -72,13 +73,30 @@ export class GameComponent implements OnInit {
 
   randomMath() {
     if (this.game.dealedCards.length < 52) {
-      return 30 * Math.random(); 
+      return 30 * Math.random();
     } else {
       return 10;
     }
   }
 
-  // designCardsAfterDealing() {
-  //   document.getElementById('card_15').style.left = "32px";
-  // }
+  designCardsAfterDealing() {
+
+    console.log('design Here');
+
+    // for (let i = 0; i < this.game.dealedCards.length; i++) {
+    //   let card_i = document.getElementById(`card_` + i);
+    // }
+
+    //   let card1 = document.getElementById('card_1');
+    //   let card51 = document.getElementById('card_1');
+    //   let card50 = document.getElementById('card_1');
+    //   let card49 = document.getElementById('card_1');
+
+
+    //   card1.style.left = "32px";
+    //   card51.style.top = "32px";
+    //   card50.style.top = "32px";
+    //   card49.style.top = "32px";
+    // }
+  }
 }
