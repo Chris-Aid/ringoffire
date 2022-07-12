23cbf3e7d25676ea850760c0b2794c9133561f98
Christian Aidelsburger <c.aidelsburger@gmx.de>
2022-07-12 07:40:45 +0200

    installed firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-c149d',
+    appId: '1:228131808193:web:e955cd653d894919230540',
+    storageBucket: 'ring-of-fire-c149d.appspot.com',
+    apiKey: 'AIzaSyAjAjb5D7-v3LpZxjXET1S8ixRo-ggbu5s',
+    authDomain: 'ring-of-fire-c149d.firebaseapp.com',
+    messagingSenderId: '228131808193',
+    measurementId: 'G-35N3M0Z6W6',
+  },
   production: true
 };
